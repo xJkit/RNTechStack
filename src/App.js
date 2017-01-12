63df21e61ff8a658ff4401f371beba455568c88f
joey-mbpr <xjkit54780@gmail.com>
2017-01-12 09:43:42 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.containerStyle}>
+        <Text style={styles.textStyle}>Tech Stack</Text>
+      </View>
+    );
+  }
+}
+
+
+const styles = {
+  containerStyle: {
+    backgroundColor: 'purple',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 32,
+  }
+};
+
+export default App;
